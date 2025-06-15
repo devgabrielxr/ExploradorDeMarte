53d815df594c8c501f694f8a480bcebbb11558f0
Gabriel Xavier Rodrigues <gabrielxavier2.0@hotmail.com>
2025-06-15 16:29:52 -0300

    Teste comando girar a direita

---
+++ ExploradorDeMarte.Testes/Comandos/ComandoGirarDireitaTestes.cs
@@ -0,0 +1,36 @@
+using ExploradorDeMarte.API.Dominio.Comandos.Interfaces;
+using ExploradorDeMarte.API.Dominio.DTOs;
+using ExploradorDeMarte.API.Dominio.Entidades.Interfaces;
+using ExploradorDeMarte.API.Dominio.Enumeradores;
+using ExploradorDeMarte.API.Dominio.Fabricas;
+using Moq;
+
+namespace ExploradorDeMarte.Testes.Comandos;
+
+public class ComandoGirarDireitaTestes
+{
+    [Fact(DisplayName = "Fábrica deve criar comando de girar à direita e executar corretamente")]
+    public void Executar_DeveGirarSondaParaEsquerda()
+    {
+        // Arrange
+        var dto = new SondaDTO
+        {
+            Nome = "Sonda Teste",
+            X = 1,
+            Y = 1,
+            Direcao = eDirecao.Norte
+        };
+
+        var planalto = new Mock<IPlanalto>().Object;
+        var outrasSondas = new List<ISonda>();
+        var sonda = FabricaDeSonda.Criar(dto);
+        IComandoSonda comando = FabricaDeComandoSonda.Criar('R');
+
+        // Act
+        comando.Executar(sonda, planalto, outrasSondas);
+
+        // Assert
+        Assert.Equal(eDirecao.Leste, sonda.Direcao);
+    }
+
+}
